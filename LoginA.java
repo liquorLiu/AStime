a374c557ccad98e69f343a8b21b0ce0ecb0fd446
liquorLiu <125868659+liquorLiu@users.noreply.github.com>
2023-02-26 21:56:59 +0800

    Add files via upload

--- LoginA.java
+++ LoginA.java
@@ -1,25 +1,58 @@
-package com.example.myapplication;
+package com.example.dailydo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
-public class LoginA extends AppCompatActivity implements View.OnClickListener{
+public class LoginA extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText id;
+    private EditText name;
+    private EditText clas;
+    private EditText phone;
+    private SharedPreferences mainActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        findViewById(R.id.login).setOnClickListener(this);
+        id = findViewById(R.id.userID);
+        name = findViewById(R.id.userName);
+        clas = findViewById(R.id.userClass);
+        phone = findViewById(R.id.userPhone);
+
+        findViewById(R.id.userBut).setOnClickListener(this);
+
+        mainActivity = getSharedPreferences("MainActivity", Context.MODE_PRIVATE);
     }
 
-        @Override
-        public void onClick(View view) {
-            Intent intent = new Intent(this,MainActivity3.class);
-            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
-            startActivity(intent);
-        }
+    @Override
+    public void onClick(View view) {
+        String ID = id.getText().toString();
+        String NAME = name.getText().toString();
+        String CLAS = clas.getText().toString();
+        String PHONE = phone.getText().toString();
 
+
+        SharedPreferences.Editor editor = mainActivity.edit();
+        editor.putString("ID", ID);
+        editor.putString("NAME", NAME);
+        editor.putString("CLAS", CLAS);
+        editor.putString("PHONE", PHONE);
+        editor.commit();
+
+
+        Intent intent = new Intent(this,MainActivity.class);
+        Bundle b =new Bundle();
+        b.putString("mingzi",name.getText().toString());
+        intent.putExtras(b);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+
+    }
 }
--- MainActivity.java
+++ MainActivity.java
@@ -1,66 +1,19 @@
-package com.example.myapplication;
+package com.example.dailydo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private TextView t;
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        t = findViewById(R.id.tv);
-        t.setText("尚未点击按钮");
-        findViewById(R.id.but_back).setOnClickListener(this);
-        Button but=findViewById(R.id.but);
-        but.setOnClickListener(new MyOnClickListener(t));
-        Button butt=findViewById(R.id.butt);
-        butt.setOnClickListener(this);
-        Button longb=findViewById(R.id.longb);
-        //匿名内部类
-        longb.setOnLongClickListener(new View.OnLongClickListener() {
-            @Override
-            public boolean onLongClick(View view) {
-                String a="你长按了这个按钮";
-                t.setText(a);
-                return false;
-            }
-        });
+        Bundle b =getIntent().getExtras();
+        String desc = b.getString("mingzi");
+        TextView name=findViewById(R.id.name);
+        name.setText(desc);
     }
-
-    @Override
-    public void onClick(View view) {
-        String a;
-        if(view.getId()==R.id.butt) {
-            a = "这里是正序显示";
-            t.setText(a);
-        }
-        else if(view.getId()==R.id.but_back){
-            finish();
-        }
-    }
-
-    static class MyOnClickListener implements View.OnClickListener{
-        private final TextView t;
-
-        public MyOnClickListener(TextView t) {
-            this.t=t;
-        }
-
-        @Override
-        public void onClick(View view){
-            String a="示显序倒是里这";
-            t.setText(a);
-
-        }
-
-    }
-
-
 }
